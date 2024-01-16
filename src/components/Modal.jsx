1730d1e516adbc45e8a94b04c3309c6085381f5e
Arpit Goyal <100536746+arpitgoyal878@users.noreply.github.com>
2024-01-16 10:03:49 +0000

    CHANGED BUTTON PATH

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
-import Button from "./button";
+import Button from "./Button";
 
 const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   const dialog = useRef();
--- src/components/sidebar/ProjectsSidebar.jsx
+++ src/components/sidebar/ProjectsSidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Button from "../button";
+import Button from "../Button";
 export default function ProjectsSidebar({
   onStartAddProject,
   projects,
